ec33ff092c83870072a76d6a58b5cc6c06d121a5
Dadu2001 <bhaveshdadure2001@gmail.com>
2024-04-12 00:41:21 +0530

    First commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,19 +1,15 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
-import { Header } from './components/header/Header'
-import { Home } from './components/home/Home'
-import About from './components/about/About'
+import { Home } from './layout/Home';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <Header/>
-        <Home/>
-        <About/>
+        <Home />
       </div>
     </>
   )
