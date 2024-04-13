ccec4e8a82fb9cc14f6f205be73c57c6d89b3203
Dadu2001 <bhaveshdadure2001@gmail.com>
2024-04-13 12:12:09 +0530

    Uploaded Files

---
+++ vite-project2/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Bar from './components/bar/Bar';
+import NavBar from './components/navbar/NavBar';
+function App() {
+  return (
+    <>
+      <div>
+      <Bar/>
+      <NavBar/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ vite-project2/src/components/bar/Bar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import arrow from "./logo.svg";
+const Bar = () => {
+  return (
+    <div>
+       <div className="bg-gradient-to-r from-purple-100 via-pink-200 to-blue-200  py-2 text-center ">
+      <div className="flex flex-row justify-center items-center font-custom">
+        <p className="hidden md:block ">
+          This page is included in a free SaaS Website Kit.
+        </p>
+        <span className="mx-2 flex flex-row ">
+          View the complete Kit <img src={arrow} />
+        </span>
+      </div>
+    </div> 
+    </div>
+  )
+}
+
+export default Bar
